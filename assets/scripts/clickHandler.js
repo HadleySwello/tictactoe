f37c748bfefb179cb6cfc94ac294746450b6cc46
hadleyana <hadleysweck@gmail.com>
2017-08-03 09:31:49 -0400

    Turn off my click functions after use
    Now it wont overwrite the already placed x's and o's.

--- assets/scripts/clickHandler.js
+++ assets/scripts/clickHandler.js
@@ -9,95 +9,102 @@ $(function counter () {
   })
 })
 
-if ($('.item').html() === '.') {
-  $(function onClick () {
-    $('#1').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#1').html('O')
-      } if (count % 2 !== 0) {
-        $('#1').html('X')
-      }
-      checkForWin.winConditions()
-    })
+$(function onClick () {
+  $('#1').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#1').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#1').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#2').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#2').html('O')
-      } if (count % 2 !== 0) {
-        $('#2').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#2').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#2').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#2').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#3').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#3').html('O')
-      } if (count % 2 !== 0) {
-        $('#3').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#3').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#3').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#3').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#4').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#4').html('O')
-      } if (count % 2 !== 0) {
-        $('#4').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#4').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#4').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#4').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#5').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#5').html('O')
-      } if (count % 2 !== 0) {
-        $('#5').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#5').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#5').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#5').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#6').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#6').html('O')
-      } if (count % 2 !== 0) {
-        $('#6').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#6').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#6').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#6').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#7').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#7').html('O')
-      } if (count % 2 !== 0) {
-        $('#7').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#7').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#7').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#7').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#8').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#8').html('O')
-      } if (count % 2 !== 0) {
-        $('#8').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#8').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#8').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#8').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-  $(function onClick () {
-    $('#9').on('click', function (event) {
-      if (count % 2 === 0) {
-        $('#9').html('O')
-      } if (count % 2 !== 0) {
-        $('#9').html('X')
-      }
-      checkForWin.winConditions()
-    })
+})
+$(function onClick () {
+  $('#9').on('click', function (event) {
+    if (count % 2 === 0) {
+      $('#9').html('O').off()
+    }
+    if (count % 2 !== 0) {
+      $('#9').html('X').off()
+    }
+    checkForWin.winConditions()
   })
-}
+})
