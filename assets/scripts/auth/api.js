d4fcec7aeed8cc2c848a59f93711f2a6db0f5af0
hadleyana <hadleysweck@gmail.com>
2017-08-03 20:25:09 -0400

    New API settings
    Attempted to fix the api

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -2,13 +2,13 @@
 
 const signUp = (data) => {
   return $.ajax({
-    //url: 'https://tic-tac-toe.wdibos.com/sign-up',
-    url: 'http://http://localhost:4741/sign-up',
+    url: 'http://tic-tac-toe.wdibos.com/sign-up',
+    // url: 'http://localhost:4741/sign-up',
     method: 'POST',
     data: {
       'credentials': {
         'email': data.credentials.email,
-        'password': data.credentials.Password,
+        'password': data.credentials.password,
         'password_confirmation': data.credentials.password
       }
     }
@@ -17,28 +17,32 @@ const signUp = (data) => {
 
 const signIn = (data) => {
   return $.ajax({
-    //url: 'https://tic-tac-toe.wdibos.com/sign-in',
-    url: 'http://http://localhost:4741/sign-in',
+    url: 'http://tic-tac-toe.wdibos.com/sign-in',
+    // url: 'http://localhost:4741/sign-in',
     method: 'POST',
     data: {
       'credentials': {
         'email': data.credentials.email,
-        'password': data.credentials.Password
+        'password': data.credentials.password
       }
     }
   })
 }
 
 const changePassword = (data) => {
+  console.log(data)
   return $.ajax({
-    url: 'https://tic-tac-toe.wdibos.com/change-password',
-    method: 'PATCH',
-    data: {
-      'passwords': {
-        'old': data.Password.old,
-        'new': data.password.new
-      }
-    }
+    // url: 'https://tic-tac-toe.wdibos.com/change-password',
+    url: app.host + '/change-password' + app.user.id,
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+    method: 'PATCH'
+    // data: {
+    //   'passwords': {
+    //     'old': data.password.old,
+    //     'new': data.password.new
+    //   }
   })
 }
 
--- assets/scripts/game-win.js
+++ assets/scripts/game-win.js
@@ -36,6 +36,8 @@ const winConditions = function (id, player) {
     message('Player One Wins!')
   } else if ($('#3').html() === 'O' && $('#5').html() === 'O' && $('#7').html() === 'O') {
     message('Player Two Wins!')
+  } else if (count === 9) {
+    message('Cats Game')
   }
 }
 
