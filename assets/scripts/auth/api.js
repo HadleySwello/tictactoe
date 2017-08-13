f3e1b890ad9590c10650f9a3052c9e6fabd44da9
hadleyana <hadleysweck@gmail.com>
2017-08-13 13:56:01 -0400

    url

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -100,7 +100,7 @@ const updateGame = (index, val, over) => {
 }
 const getHistory = (data) => {
   return $.ajax({
-    url: app.host + '/games/' + data.games.id,
+    url: app.host + '/games/' + data.game.id,
     method: 'GET',
     headers: {
       Authorization: 'Token token=' + app.user.token
